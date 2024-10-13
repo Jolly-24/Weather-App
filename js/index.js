c33b6899c2e3a92fa05a358ec15ec06c37e5d824
Jolly Nessem <jollynessem@gmail.com>
2024-10-13 17:34:07 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ async function getWeather(countryName="cairo"){
     const response = await fetch(`https://api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`);
     let locationData = await response.json();
     console.log(locationData.city);  
-    getWeather("locationData.city")
+    getWeather(locationData.city)
   }
   
 function getTodayData(data){
